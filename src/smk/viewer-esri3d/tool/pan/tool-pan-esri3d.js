dc8f40f359d68433a3dc2051412c28f83588a909
Benjamin Jubb <benjubb@gmail.com>
2021-01-12 11:18:53 -0800

    fix esri3d pan tool

--- src/smk/viewer-esri3d/tool/pan/tool-pan-esri3d.js
+++ src/smk/viewer-esri3d/tool/pan/tool-pan-esri3d.js
@@ -1,7 +1,7 @@
 include.module( 'tool-pan-esri3d', [ 'tool-pan', 'esri3d' ], function () {
     "use strict";
 
-    SMK.TYPE.PanTool.prototype.afterInitialize.push( function ( smk ) {
+    SMK.TYPE.PanTool.addInitializer( function ( smk ) {
         smk.$viewer.view.ui.add( [
             {
                 component: new SMK.TYPE.Esri3d.widgets.NavigationToggle( { view: smk.$viewer.view } ),
