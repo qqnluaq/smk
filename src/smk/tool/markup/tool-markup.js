df6803a8f68878aea93d5daff545b901062fcb35
Benjamin Jubb <bjubb@vividsolutions.com>
2021-09-07 15:41:24 -0700

    update markup lib to v2.11.2, restructure tool, so that each instance draws a different shape

--- src/smk/tool/markup/tool-markup.js
+++ src/smk/tool/markup/tool-markup.js
@@ -1,5 +1,38 @@
-include.module( 'tool-markup', [ 'tool' ], function () {
+include.module( 'tool-markup', [ 
+    'tool',
+    'tool-widget'
+], function ( inc ) {
     "use strict";
 
-    return SMK.TYPE.Tool.define( 'MarkupTool' )
+    Vue.component( 'markup-widget', {
+        extends: SMK.COMPONENT.ToolWidgetBase,
+    } )
+
+    return SMK.TYPE.Tool.define( 'MarkupTool', 
+        function () {
+            SMK.TYPE.ToolWidget.call( this, 'markup-widget' )
+        
+            this.defineProp( 'drawMode' )
+        },
+        function ( smk ) {
+            var self = this
+        
+            smk.on( this.id, {
+                'activate': function () {
+                    if ( !self.enabled ) return
+                },
+            } )
+
+            this.changedActive( function () {
+                if ( self.active ) {
+                    SMK.HANDLER.get( self.id, 'activated' )( smk, self )                    
+                }
+                else {
+                    SMK.HANDLER.get( self.id, 'deactivated' )( smk, self )
+                }
+            } )
+
+            SMK.HANDLER.get( self.id, 'initialized' )( smk, self )    
+        }
+    )
 } )
